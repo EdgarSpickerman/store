e628b38ada1d3c13c2fb76b5b4bb9ccecb0e4ab3
Edgar Spickerman <spickerman.edgar@gmail.com>
2018-04-19 12:08:21 -0700

    added order check for customer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,19 +14,20 @@ const questions = {
     name: "id",
     message: "Please select which product id you would like to purchase",
     validate: (choice) => {
-      if(Number.isInteger(parseInt(choice))) return true;
+      if (Number.isInteger(parseInt(choice))) return true;
       return "Please enter a valid item_id."
     }
-  },{
+  }, {
     type: "prompt",
     name: "qty",
     message: "Please select how many of these products you would like to purchase",
     validate: (choice) => {
-      if(Number.isInteger(parseInt(choice))) return true;
+      if (Number.isInteger(parseInt(choice))) return true;
       return "Please enter a valid quantity."
     }
   }]
 }
+
 
 const getCatalog = () => {
   const statement = `select * from products where stock_quantity > 0`;
@@ -35,10 +36,29 @@ const getCatalog = () => {
   })
 }
 
+const checkOrder = (order) => {
+  const statement = `select * from products where item_id = ${order.id} and stock_quantity >= ${order.qty}`;
+  return new Promise((resolve, reject) => {
+    conn.query(statement, (err, res) =>
+      err ? reject(err): resolve({ items: res, order: order }))
+  })
+}
+
+const processOrder = (order)=>{
+  return checkOrder(order)
+    .then(results=>{
+      if(results.items.length){
+        return "is good"
+      }
+      return "Insufficient quantity!";
+    })
+}
+
 conn.connect()
 getCatalog()
   .then(catalog => console.table(catalog))
-  .then(()=>inquire.prompt(questions.order))
-  .then(order=>console.log(order))
+  .then(() => inquire.prompt(questions.order))
+  .then(order => processOrder(order))
+  .then(status => console.log(status))
   .then(() => conn.end())
   .catch(err => console.log(err))
